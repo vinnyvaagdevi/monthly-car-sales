88f24027bde85fd094cd5173040f698ea52e9e30
vinnyvaagdevi <96054101+vinnyvaagdevi@users.noreply.github.com>
2021-12-27 16:44:14 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import joblib
-st.title('MONTHLY CAR SALES PREDICTION')
-op = joblib.load('monthly_carsales')
-ip = st.slider("pick number of months",0,100)
-fig = op.predict([ip])
-if st.button('Predict'):
-   st.pyplot(fig[0]) 
