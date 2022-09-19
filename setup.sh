9e255f375f23566e6e50cdda790606380c84cda4
vinnyvaagdevi <96054101+vinnyvaagdevi@users.noreply.github.com>
2022-09-19 20:39:55 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
-nkdir -p ~/.streamlit/
+mkdir -p ~/.streamlit/
+
 echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
--> ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml
